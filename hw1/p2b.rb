edf59554298a3ea2cf73c0e6cdf23ad3f323e03b
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2025-09-10 16:54:42 -0300

    add hw1/p2b.rb

---
+++ hw1/p2b.rb
@@ -0,0 +1,37 @@
+class WrongNumberOfPlayersError < StandardError ; end
+class NoSuchStrategyError < StandardError ; end
+def rps_game_winner(game)
+  raise WrongNumberOfPlayersError unless game.length == 2
+
+  p1 = [game[0][0], game[0][1].to_s.upcase]
+  p2 = [game[1][0], game[1][1].to_s.upcase]
+  
+	raise NoSuchStrategyError unless %w[R P S].include?(p1[1]) && %w[R P S].include?(p2[1])
+
+  return p1 if p1[1] == p2[1]
+  wins = { "R" => "S", "S" => "P", "P" => "R" }
+  wins[p1[1]] == p2[1] ? p1 : p2
+end
+
+def rps_tournament_winner(tournament)
+  if tournament[0].is_a?(String)
+    return tournament
+  end
+
+  left_winner = rps_tournament_winner(tournament[0])
+  right_winner = rps_tournament_winner(tournament[1])
+  rps_game_winner([left_winner, right_winner])
+end
+
+t = [
+  [
+    [["Kristen", "P"], ["Dave", "S"]],
+    [["Richard", "R"], ["Michael", "S"]]
+  ],
+  [
+    [["Allen", "S"], ["Omer", "R"]],
+    [["David E.", "R"], ["Richard X.", "P"]]
+  ]
+]
+
+p rps_tournament_winner(t)
