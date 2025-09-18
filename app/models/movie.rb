9f6e0a02a2af8629d0e626f94c58bd0f2b2b5712
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2025-09-18 10:58:50 -0300

    feat:validacoes no modelo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
-  def self.all_ratings
-    %w[G PG PG-13 R NC-17]
-  end
+  def self.all_ratings = %w[G PG PG-13 R NC-17]
+
+  validates :title, presence: true, length: { maximum: 120 }
+  validates :rating, inclusion: { in: all_ratings }
+  validates :release_date, presence: true
 end
